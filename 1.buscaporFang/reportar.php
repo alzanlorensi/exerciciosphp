a994ac7924f2ca89545413662bcbbfd047f6b293
André Zanlorensi <Andr Zanlorensi@DESKTOP-JJAU6V7>
2018-01-23 15:32:16 -0200

    adicionei a interação  com o DB

--- 1.buscaporFang/reportar.php
+++ 1.buscaporFang/reportar.php
@@ -4,26 +4,26 @@
 
 <body>
      <h2 style="color: blue; font-style: italic; font-family: Calibri">Alianígenas me abduziram- Relate uma abdução.</h2>
-
+</body>
+</html>
 <?php
+$db= mysqli_connect('localhost', 'root' ,'' , 'exercicios') or die("Erro ao conectar com o servidor");
 
-$nome_completo= $_POST['primeironome'].' '.$_POST['sobrenome'];
+$nome= $_POST['primeironome'];
+$sobrenome= $_POST['sobrenome'];
 $quando_aconteceu= $_POST['quando'];
 $quanto_tempo= $_POST['tempofora'];
 $descricao_alien= $_POST['descricao'];
 $fang_visto= $_POST['verFang'];
 $email= $_POST['email'];
+$quantosaliens= $_POST['quantos'];
+$fizeram= $_POST['fizeram'];
+$add= $_POST['algo'];
+$query= "INSERT INTO `abducao`(`primeiro_nome`, `sobrenome`, `aconteceu`, `quanto_tempo`, `quantos`, `descricao`, `fizeram`, `fang`, `outro`, `email`) VALUES ('$nome','$sobrenome','$quando_aconteceu','$quanto_tempo','$quantosaliens','$descricao_alien','$fizeram','$fang_visto','$add','$email')";
+echo"$nome foi abduzido $quando_aconteceu e ficou fora por $quanto_tempo <br>\n"
+        ."Descrição do Alien: $descricao_alien <br>"
+        . "Os aliens estavam num total de: $quantosaliens <br>"
+        ."Eles fizeram: $fizeram <br> Voce viu o Fang? $fang_visto <br> Tem mais algo a dizer?: $add";
+$result= mysqli_query($db, $query) or die('Deu ruim clã');
+mysqli_close($db);
 
-echo 'Obrigado por enviar o formulário '.$nome_completo.'<br />';
-echo 'Você foi abduzido '.$quando_aconteceu;
-echo 'e se foir por '.$quanto_tempo.'<br>';
-echo 'Descreva eles: '.$descricao_alien. '<br>';
-echo 'Fang estava lá ?'.$fang_visto.'<br>';
-echo 'Seu email é:'.$email.'<br>';
-echo 'Os aliens fizeram: '.$_POST['fizeram'].'<br>';
-echo 'Quantos eram? '.$_POST['quantos'].'<br>';
-echo "Informações adicionais: ".$_POST['algo'];
-?>
-
-</body>
-</html>
