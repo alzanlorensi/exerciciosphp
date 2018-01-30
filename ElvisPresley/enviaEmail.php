bdc1cc18b43b174e00d5848db8e298303eb3ce59
Abc <abc@df.com>
2018-01-30 11:14:41 -0200

    Finalizei o exercicio chato do Elvis

--- ElvisPresley/enviaEmail.php
+++ ElvisPresley/enviaEmail.php
@@ -1,11 +1,44 @@
 <?php
- $db= mysqli_connect('localhost', 'root', '1234', 'exercicios') or die("Não deu boa");
+ if(isset($_POST['Enviar'])){
  $assunto= $_POST["assunto"];
  $mensagem= $_POST["mensagem"];
- $query= "SELECT * from elvis;";
- $result= mysqli_query($db, $query);
- while ($row= mysqli_fetch_array($result)){
-     echo $row['nome'].' '.$row['sobrenome'].' '.$row['email'].'<br>';
- }
- 
- mysqli_close($db);
+ $output_form= false;
+ if (empty($mensagem) or empty($assunto)){
+     echo "Você Esqueceu de alguns campos";
+     $output_form= true;
+ } else {
+     $db= mysqli_connect('localhost', 'root', '1234', 'exercicios') or die("Não deu boa");
+     $query= "SELECT * from elvis;";
+    $result= mysqli_query($db, $query);
+    echo "Email enviado com sucesso";
+    mysqli_close($db);
+}
+ } else {
+    $output_form=true;
+}
+if ($output_form){
+    ?>
+<html><form method="POST" actio="<?php echo $_SERVER['PHP_SELF'];?>" >
+            <table border="0" cellspacing="1" align="center">
+                <tbody>
+                    <tr>
+                        <td id="label">Assunto:</td>
+                        <td><input type="text" name="assunto" size="46"/></td>
+                    </tr>
+                    <tr>
+                        <td id="label"> Mensagem:</td>
+                        <td><textarea name="mensagem" style="width: 300px; height: 210px;" placeholder="Digite a mensagem aqui."></textarea></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="center" ><input type="submit" value="Enviar" id="button"/></td>
+                    </tr>
+                </tbody>
+            </table>
+            <div> <img src="../img/elvisPNG.png" width="359" height="392" alt="elvisPNG" align="right"/>
+            </div>
+            
+            
+       </form></html>
+<?php
+}
+ ?>
