3f01ee9e1700c31d3301eebedac5e4aa5ff71f29
André Zanlorensi <Andr Zanlorensi@DESKTOP-JJAU6V7>
2018-01-30 18:59:38 -0200

    inicio do exercicio do Elvis....

--- guitarScore/cadastraScore.php
+++ guitarScore/cadastraScore.php
@@ -1,16 +1,40 @@
-<html>
-    <h2>Guitar War- Score</h2>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-        
-    </form>
-    
+<html xlmns="http://wwww.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <title>Guitar War</title>
+        <link rel="shortcut icon" href="../img/guitar.ico" type="image/x-icon">
+    </head>
+    <body>
+        <h2>Guitar War- Score</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+
+            <?php
+            if (isset($_POST['enviar'])) {
+                $nome = $_POST['nome'];
+                $score = $_POST['score'];
+                if (!empty($name) && empty($score)) {
+                    $db = mysqli_connect('localhost', 'root', '1234', 'exercicios') or die("Deu ruim");
+                    $query = "INSERT INTO guitarwar values (0,'$name', NOW(),'$score'); ";
+                }
+            }
+            ?>
+
+
+            <hr/>
+            <table border="1" align="center">
+                <tbody>
+                    <tr>
+                        <td>Nome:</td>
+                        <td><input type="text" id="nome" name="nome" value="<?php if (!empty($name)) echo "$name"; ?>"><br></td>
+                    </tr>
+                    <tr>
+                        <td>Score</td>
+                        <td><input type="text" value="<?php if (!empty($score)) echo $score; ?>"</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="submit" value="Enviar" name="enviar" id="enviar"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </body>
 </html>
-
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
