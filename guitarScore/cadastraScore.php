15bd5613b167aca5f2ad82cf48ced94e4c720069
Abc <abc@df.com>
2018-01-30 12:21:56 -0200

    Netbeans esta fechando mudarei para o Windows

---
+++ guitarScore/cadastraScore.php
@@ -0,0 +1,16 @@
+<html>
+    <h2>Guitar War- Score</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        
+    </form>
+    
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
